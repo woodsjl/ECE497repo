142200144c9bb2d8a2983b968c8a20615622cb99
woodsjl <joshualwoods@gmail.com>
2016-09-09 00:42:30 +0000

    buttonsAndLEDs is working and this is the commit of it

--- hw02/ReadMe.md
+++ hw02/ReadMe.md
@@ -1,3 +1,4 @@
 Josh Woods 09/09/2016
 Homework 02 for ECE 497
-1. I have flashe
+1. buttonsAndLEDS.js toggles LED's when the corresponding button is pressed
+	
--- hw02/buttonsAndLEDs.js
+++ hw02/buttonsAndLEDs.js
@@ -3,29 +3,72 @@
 // this is for hw02 
 // When a button is pressed light up a corresponding LED
 
+// initialize all of the need variables 
 var b =  require('bonescript');
 var leftBtn = 'P9_11';
 var topBtn = 'P9_13';
-var rightBtn = 'P9_15';
+var rightBtn = 'P9_16';
 var downBtn = 'P9_17';
 var led0 = 'P9_23';
-var led1 = 'P9_25';
+var led1 = 'P9_24';
 var led2 = 'P9_27';
 var led3 = 'P9_41';
+var value0 = 0;
+var value1 = 0;
+var value2 = 0;
+var value3 = 0;
 
-b.pinMode(topBtn, b.INPUT,7,'pulldown');
+// set button pins to input
+b.pinMode(topBtn, b.INPUT);
+b.pinMode(leftBtn, b.INPUT);
+b.pinMode(downBtn, b.INPUT);
+b.pinMode(rightBtn, b.INPUT);
 
+// set button pins to output
 b.pinMode(led0,b.OUTPUT);
+b.pinMode(led1,b.OUTPUT);
+b.pinMode(led2,b.OUTPUT);
+b.pinMode(led3,b.OUTPUT);
 
-b.digitalWrite(led0, 0);
+// initialize interrupts for buttons
+b.attachInterrupt(topBtn, setLED0, b.RISING);
+b.attachInterrupt(rightBtn, setLED1, b.RISING);
+b.attachInterrupt(downBtn, setLED2, b.RISING);
+b.attachInterrupt(leftBtn, setLED3, b.RISING);
 
-b.attachInterrupt(topBtn,true,b.CHANGE, changeState(led0));
+console.log("running");
 
-
-function changeState(var led) {
-	if (b.digitalRead(led) === 0) {
-		b.digitalWrite(led,1);
+// event handler functions 
+function setLED0() {
+	if(value0 === 1) {
+		value0 = 0;
 	} else {
-		b.digitalWrite(led,0);
+		value0 = 1;
 	}
+    b.digitalWrite(led0, value0);
 }
+function setLED1() {
+	if(value1 === 1) {
+		value1 = 0;
+	} else {
+		value1 = 1;
+	}
+    b.digitalWrite(led1, value1);
+}
+function setLED2() {
+	if(value2 === 1) {
+		value2 = 0;
+	} else {
+		value2 = 1;
+	}
+    b.digitalWrite(led2, value2);
+}
+function setLED3() {
+	if(value3 === 1) {
+		value3 = 0;
+	} else {
+		value3 = 1;
+	}
+    b.digitalWrite(led3, value3);
+}
+
